6d1d7fa8d2d8f88ce7d2aaf73ad4822570393d31
Sathya Seelan <sathyaseelangunasekar@gmail.com>
2024-02-06 12:30:54 +0530

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Home from "./pages/Home";
+import ReactGA from 'react-ga';
+import React, {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-SW2KGM0L12');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
   return (
       <Home />
   );
